e4ab40afe516058ef10c17ee7218f5a774bcd407
p2wna22002 <126845591+p2wna22002@users.noreply.github.com>
2023-04-03 10:24:18 +0530

    Update Mysqlfeed.py

--- RPI_LAB/Mysqlfeed.py
+++ RPI_LAB/Mysqlfeed.py
@@ -4,14 +4,14 @@
 import pymysql
 #Create a connection to MySQL Database 
 # conn =pymysql.connect(database="databasename",user="user",password="password",host="localhost")
-conn =pymysql.connect(database="IOTData",user="pi",password="raspberry",host="http://34.100.152.74/phpmyadmin")
+conn =pymysql.connect(database="IOTData",user="pi",password="raspberry",host="localhost")
 #Create a MySQL Cursor to that executes the SQLs
 cur=conn.cursor()
 #Create a dictonary containing the fields, name, age and place
 # data={'name':'hello','age':10,'place':'kollam'}
 data={'Topic':'Sensor/TEMP','Data':'35'}
 #Execute the SQL to write data to the database
-cur.execute("INSERT INTO <MQTTData>(Topic, Data)VALUES(%(Topic)s,%(Data)s);",data)
+cur.execute("INSERT INTO MQTTData (Topic, Data)VALUES(%(Topic)s,%(Data)s);",data)
 #Close the cursor
 cur.close()
 #Commit the data to the database
