f9a2bb7bc1e07accfa3954039319116145020be2
p2wna22002 <126845591+p2wna22002@users.noreply.github.com>
2023-04-21 21:01:24 +0530

    Update MQTTsql.py

--- RPI_LAB/MQTTsql.py
+++ RPI_LAB/MQTTsql.py
@@ -1,1 +1,46 @@
+import paho.mqtt.client as mqtt
+import mysql.connector
+
+# MQTT broker settings
+broker_address = "mqtt.example.com"
+broker_port = 1883
+topic = "sensors/data"
+
+# MySQL database settings
+db_host = "localhost"
+db_user = "root"
+db_password = "password"
+db_name = "sensors"
+
+# Connect to the MQTT broker
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker with result code "+str(rc))
+    client.subscribe(topic)
+
+# Process incoming messages
+def on_message(client, userdata, msg):
+    print("Received message on topic "+msg.topic+" with payload "+msg.payload.decode())
+    sensor_data = float(msg.payload.decode())
+
+    # Connect to the MySQL database
+    db = mysql.connector.connect(host=db_host, user=db_user, password=db_password, database=db_name)
+    cursor = db.cursor()
+
+    # Insert sensor data into the database
+    sql = "INSERT INTO sensor_data (value) VALUES (%s)"
+    val = (sensor_data,)
+    cursor.execute(sql, val)
+    db.commit()
+    print(cursor.rowcount, "record inserted.")
+
+    # Close the database connection
+    cursor.close()
+    db.close()
+
+# Connect to the MQTT broker and start the main loop
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect(broker_address, broker_port)
+client.loop_forever()
 
