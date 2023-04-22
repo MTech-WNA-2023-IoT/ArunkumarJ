1326dc5b14100823ddb285c35f4a33e00acf1a4a
p2wna22002 <126845591+p2wna22002@users.noreply.github.com>
2023-04-22 21:45:29 +0530

    Update mqttmysql.py

--- RPI_LAB/mqttmysql.py
+++ RPI_LAB/mqttmysql.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as paho
+import pymysql
 #pip3 install paho-mqtt
 global mqttclient;
 global broker;
@@ -20,6 +21,25 @@ def test(client, userdata, message):
 def _on_message(client, userdata, msg):
 # 	print("Received: Topic: %s Body: %s", msg.topic, msg.payload)
 	print(msg.topic+" "+str(msg.payload))
+
+#Create a connection to MySQL Database 
+# conn =pymysql.connect(database="databasename",user="user",password="password",host="localhost")
+conn =pymysql.connect(database="IOTData",user="pi",password="raspberry",host="localhost")
+#Create a MySQL Cursor to that executes the SQLs
+cur=conn.cursor()
+#Create a dictonary containing the fields, name, age and place
+# data={'name':'hello','age':10,'place':'kollam'}
+# data={`ID`:'7','TimeStamp':current_timestamp(),'Topic':'Sensor/TEMP','Data':'40'}
+#Execute the SQL to write data to the database
+cur.execute("INSERT INTO `MQTTData` (`ID`, `TimeStamp`, `Topic`, `MyData`) VALUES ('8', current_timestamp(), 'Sensor/TEMP', '38');")
+#Close the cursor
+cur.close()
+#Commit the data to the database
+conn.commit()
+#Close the connection to the database
+conn.close()
+
+#Open phpMyAdmin and see how the data is stored to the database
 	 
 #Subscribed Topics 
 def _on_connect(mqttclient, userdata, flags, rc):
