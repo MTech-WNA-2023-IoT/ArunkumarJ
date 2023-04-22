6d70afe96d5187a29fb99b455302a8bd7b89d1db
p2wna22002 <126845591+p2wna22002@users.noreply.github.com>
2023-04-22 20:59:47 +0530

    Update Mysqlfeed.py

--- RPI_LAB/Mysqlfeed.py
+++ RPI_LAB/Mysqlfeed.py
@@ -4,7 +4,7 @@
 import pymysql
 #Create a connection to MySQL Database 
 # conn =pymysql.connect(database="databasename",user="user",password="password",host="localhost")
-conn =pymysql.connect(database="IOTData",user="pi",password="raspberry",host="34.93.203.151:3306")
+conn =pymysql.connect(database="IOTData",user="pi",password="raspberry",host="0.0.0.0:3306")
 #Create a MySQL Cursor to that executes the SQLs
 cur=conn.cursor()
 #Create a dictonary containing the fields, name, age and place
