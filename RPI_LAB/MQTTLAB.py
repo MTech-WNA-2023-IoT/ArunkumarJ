526db6349b04133a9933a7b0f40a0873d3dbe60a
p2wna22002 <126845591+p2wna22002@users.noreply.github.com>
2023-05-20 11:04:56 +0530

    Update MQTTLAB.py

--- RPI_LAB/MQTTLAB.py
+++ RPI_LAB/MQTTLAB.py
@@ -44,7 +44,7 @@ def _on_message(client, userdata, msg):
 def _on_connect(mqttclient, userdata, flags, rc):
 # 	print("New Client: "+str(mqttclient)+ " connected")
 # 	print(rc)
-	mqttclient.subscribe("IOT/#", qos=0)	
+	mqttclient.subscribe("SENSOR/#", qos=0)	
   
 mqttclient.message_callback_add("SENSOR/TEMP", test)
 
